47d36414410824b8f70cbae3d7287e705cbbc083
1Sahana-rmk <241062.CD@rmkec.ac.in>
2026-02-18 17:10:50 +0530

    Create sentiment_dictionary.py

---
+++ sentiment_dictionary.py
@@ -0,0 +1,13 @@
+# sentiment_dictionary.py
+
+word_scores = {
+    "excellent": 5,
+    "amazing": 4,
+    "good": 3,
+    "nice": 2,
+    "average": 1,
+    "okay": 0,
+    "bad": -3,
+    "poor": -4,
+    "worst": -5
+}
